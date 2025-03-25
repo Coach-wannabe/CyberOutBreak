b9a41227c526f877f34c8eae8ea78641e02620fe
OrkGhazghkullThraka <85020618+Coach-wannabe@users.noreply.github.com>
2025-03-25 19:03:47 +0500

    smoothed movement and added sprites

--- Assets/Scripts/Game/Player/PlayerMovement.cs
+++ Assets/Scripts/Game/Player/PlayerMovement.cs
@@ -10,6 +10,8 @@ public class PlayerMovement : MonoBehaviour
 
     private Rigidbody2D _rigidbody;
     private Vector2 _movementInput;
+    private Vector2 _smoothedMovementInput;
+    private Vector2 _movementInputSmoothedVelocity;
 
     private void Awake()
     {
@@ -18,7 +20,12 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        _rigidbody.linearVelocity = _movementInput * _speed;
+        _smoothedMovementInput = Vector2.SmoothDamp(
+            _smoothedMovementInput,
+            _movementInput,
+            ref _movementInputSmoothedVelocity,
+            0.1f);
+        _rigidbody.linearVelocity = _smoothedMovementInput * _speed;
     }
 
     private void OnMove(InputValue inputValue)
