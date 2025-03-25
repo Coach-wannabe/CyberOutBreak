90992d3dc890603f9464e4e64e9b4068f61f21cc
OrkGhazghkullThraka <85020618+Coach-wannabe@users.noreply.github.com>
2025-03-25 19:19:58 +0500

    added rotation when input

--- Assets/Scripts/Game/Player/PlayerMovement.cs
+++ Assets/Scripts/Game/Player/PlayerMovement.cs
@@ -8,6 +8,9 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private float _speed;
 
+    [SerializeField]
+    private float _rotationSpeed;
+
     private Rigidbody2D _rigidbody;
     private Vector2 _movementInput;
     private Vector2 _smoothedMovementInput;
@@ -20,12 +23,29 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        SetPlayerVelocity();
+        RotateInDirectionOfMovement();
+    }
+
+    private void SetPlayerVelocity()
+    {
         _smoothedMovementInput = Vector2.SmoothDamp(
-            _smoothedMovementInput,
-            _movementInput,
-            ref _movementInputSmoothedVelocity,
-            0.1f);
+                    _smoothedMovementInput,
+                    _movementInput,
+                    ref _movementInputSmoothedVelocity,
+                    0.1f);
         _rigidbody.linearVelocity = _smoothedMovementInput * _speed;
+    }
+
+    private void RotateInDirectionOfMovement()
+    {
+        if(_movementInput != Vector2.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(transform.forward, _smoothedMovementInput);
+            Quaternion rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);
+
+            _rigidbody.MoveRotation(rotation);
+        }
     }
 
     private void OnMove(InputValue inputValue)
